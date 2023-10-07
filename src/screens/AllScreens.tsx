8a82b623571ceb3d21dc6ac4269724d0831663c0
Naranbat <massive0304@gmail.com>
2023-10-07 19:07:35 +0800

    feat: navigating all screen

--- src/screens/AllScreens.tsx
+++ src/screens/AllScreens.tsx
@@ -1,14 +1,29 @@
+import {MainNavParamList} from '@/navigations/types/MainNavParamList';
+import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-export default function AllScreens() {
+export default function AllScreens({
+  navigation,
+}: StackScreenProps<MainNavParamList, 'AllScreens'>) {
   return (
     <View style={styles.screen}>
-      <Text>AllScreens</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('NetflixCategory')}
+        style={styles.navigateButton}>
+        <Text>Netflix Category</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {flex: 1},
+  screen: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  navigateButton: {
+    width: 200,
+    height: 50,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
